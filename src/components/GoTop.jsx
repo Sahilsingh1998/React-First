076f105503b15a3991f89a65dd7d2ecddcd95985
Sahil Singh <singhsahil.8340@gmail.com>
2024-03-21 18:58:37 +0530

    update

--- src/components/GoTop.jsx
+++ src/components/GoTop.jsx
@@ -4,8 +4,6 @@ import ScrollToTop from "react-scroll-to-top";
 function GoTop() {
   return (
     <div>
-      <h1>Hello, world!</h1>
-      <div style={{ marginTop: "150vh" }} />
       <ScrollToTop smooth />
     </div>
   )
