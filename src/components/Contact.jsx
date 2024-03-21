3bdfdcdcec008407c934e6b4ad916277b399b473
Sahil Singh <singhsahil.8340@gmail.com>
2024-03-21 11:04:18 +0530

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -64,30 +64,10 @@ function Contact() {
         </section>
 
         <div className="container-fluid mt-5">
-          {/* <div class="responsive-map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7205.258830412314!2d87.24579554199615!3d25.45065026019435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f0182634c26067%3A0x26b88ea67dd3e779!2sKursela%2C%20Bihar!5e0!3m2!1sen!2sin!4v1710963292649!5m2!1sen!2sin" allowfullscreen></iframe>
-          </div> */}
+          <div class="responsive-map">
+            <iframe title="Address" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7205.258830412314!2d87.24579554199615!3d25.45065026019435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f0182634c26067%3A0x26b88ea67dd3e779!2sKursela%2C%20Bihar!5e0!3m2!1sen!2sin!4v1710963292649!5m2!1sen!2sin" allowfullscreen></iframe>
+          </div>
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         <div className="contact-icon">
           <a href='https://www.instagram.com/it_sahil_rajput?igsh=MWNoM2ZhY3UwYWJhMw==' target='blank' className="items">
